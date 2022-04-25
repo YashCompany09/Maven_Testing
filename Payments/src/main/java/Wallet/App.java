4e2da942bffafb6dc3b65a4d538109171cabe3a0
Yashwanth Kumar <yashwanth.chodipalli@gmail.com>
2022-04-25 09:48:26 +0530

    first push by Dev 01

---
+++ Payments/src/main/java/Wallet/App.java
@@ -0,0 +1,13 @@
+package Wallet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Friends!" );
+    }
+}
