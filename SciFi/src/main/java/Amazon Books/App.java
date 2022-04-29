0c98d448567c9ffe1b35246c69295bafbedf365a
Yash <chodipalliyashwanth1997@gmail.com>
2022-04-29 12:28:14 +0000

    these are the new books

---
+++ SciFi/src/main/java/Amazon Books/App.java
@@ -0,0 +1,13 @@
+package Amazon Books;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
